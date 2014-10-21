deb432d641f73b3e3052e1978f03b844d8637790
Thibault Vieux <melkir13@gmail.com>
2014-10-21 15:41:38 +0200

    Update script6.sh

--- SYS/script6.sh
+++ SYS/script6.sh
@@ -8,12 +8,12 @@ if [ $# != 1 ]; then
     exit 1
 fi
 
-if [ ! -e $1 ]; then
+if [ ! -e "$1" ]; then
     echo "File $1 not found" 1>&2
     exit 1
 fi
 
 # echo $(cat $1 | grep $USER | cut -d : -f 1,7)
 # passwd ne contenant pas de login de l'utilisateur courant, j'ai choisi de prendre etu
-echo $(cat $1 | grep etu | cut -d : -f 1,7)
+echo $(cat "$1" | grep etu | cut -d : -f 1,7)
 
