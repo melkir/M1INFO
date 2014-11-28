b179327f982b3a4e85eab7354b4ce79e3c5ba05d
melkir <melkir13@gmail.com>
2014-11-28 01:42:47 +0100

    oops fixed derp

--- POO/TD4/src/Mammifere.cpp
+++ POO/TD4/src/Mammifere.cpp
@@ -28,7 +28,7 @@ Mammifere *Mammifere::engendrer(bool isFemale) {
     }
     string name = getNom() + (isFemale ? "_fille" : "_fils");
     cout << "Je donne naissance à un Mammifere " << (isFemale ? "femelle" : "male") << endl;
-    return new Mammifere(getX(), getY(), getNom(), isFemale, getVitesse());
+    return new Mammifere(getX(), getY(), name, isFemale, m_vitesse);
 }
 
 ostream &operator<<(std::ostream &os, const Mammifere &m) {
